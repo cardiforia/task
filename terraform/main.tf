12a1ae1d663b2a7fbc99ae2e7a297940013985ae
Ventsislav Nikolov <cardiforia@gmail.com>
2025-06-15 15:48:48 +0300

    ArgoCD deployed

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,12 +4,23 @@ resource "helm_release" "argo_cd" {
   chart      = "argo-cd"
   namespace  = "argocd"
   create_namespace = true
+  version    = "5.46.2"
 }
 
-resource "kubernetes_manifest" "infrastructure_app" {
-  manifest = yamldecode(file("${path.module}/infrastructure-app.yaml"))
+resource "null_resource" "wait_for_argo_cd_crds" {
+  depends_on = [helm_release.argo_cd]
+
+  provisioner "local-exec" {
+    command = "echo 'Waiting for Argo CD CRDs...'; sleep 30"
+  }
 }
 
-resource "kubernetes_manifest" "applications_app" {
-  manifest = yamldecode(file("${path.module}/applications-app.yaml"))
+resource "kubectl_manifest" "infrastructure_app" {
+  depends_on = [null_resource.wait_for_argo_cd_crds]
+  yaml_body  = file("${path.module}/infrastructure-app.yaml")
+}
+
+resource "kubectl_manifest" "applications_app" {
+  depends_on = [null_resource.wait_for_argo_cd_crds]
+  yaml_body  = file("${path.module}/applications-app.yaml")
 }
